af42295ad7d867173695c4d34373be1d33611650
erick2990 <e30rick@gmail.com>
2025-10-22 03:59:48 -0600

    Ventanas y tablas creadas primera actualizacion

--- control_db.py
+++ control_db.py
@@ -11,12 +11,13 @@ class Taller:
     def _conn():
         conn = sqlite3.connect(DB_NAME)
         conn.row_factory =sqlite3.Row
+        return conn
 
     def crear_tablas(self):
         conn = self._conn()
         cursor = conn.cursor()
 
-        cursor.excute("""  
+        cursor.execute("""  
         CREATE TABLE IF NOT EXISTS usuarios(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT,
@@ -24,19 +25,7 @@ class Taller:
             contra TEXT
         )    
         """)
-
-        cursor.excute("""
-        CREATE TABLE IF NOT EXISTS aparatos(
-            no INTEGER PRIMARY KEY AUTOINCREMENT,
-            tipo TEXT,
-            marca TEXT,
-            modelo TEXT,
-            falla TEXT,
-            cliente TEXT
-        )
-        """)
-
-        cursor.excute("""
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS cliente(
             nit INTEGER PRIMARY KEY AUTOINCREMENT,
             nombre TEXT,
@@ -44,4 +33,36 @@ class Taller:
             direccion TEXT
         )
         """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS aparatos(
+            no INTEGER PRIMARY KEY AUTOINCREMENT,
+            tipo TEXT,
+            marca TEXT,
+            modelo TEXT,
+            falla TEXT,
+            cliente_nit TEXT
+            FOREIGN KEY(cliente_nit) REFERENCES cliente(nit)
+        )
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS bodega(
+            pieza INTEGER PRIMARY KEY AUTOINCREMENT,
+            producto TEXT,
+            modelo TEXT
+        )
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS registros_entradas(
+            registro INTEGER PRIMARY KEY AUTOINCREMENT,
+            no_aparato INTEGER,
+            id_trabajador INTEGER,
+            cliente_nit INTEGER
+            presupuesto REAL,
+            FOREIGN KEY(no_aparato) REFERENCES aparatos(no)
+            FOREIGN KEY(id_trabajador) REFERENCES usuarios(id)
+            FOREIGN KEY(cliente_nit) REFERENCES cliente(nit)
+        )
+        """)
+
 
